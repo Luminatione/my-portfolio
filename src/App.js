2c6e1835cd742ea2b9857ff79b0d61cbe56bd940
Luminatione <jakosziewski@gmail.com>
2025-05-20 00:53:28 +0200

    dependency fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "react-ui-cards";
 import { motion } from "framer-motion";
 
 const projects = [
